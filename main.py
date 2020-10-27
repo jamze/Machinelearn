1f60f7dcb25f2b63e46206d4c6006dc2bf5448c5
darek <daro1392@gmail.com>
2020-10-27 15:11:07 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,70 @@
+##  https://towardsdatascience.com/inroduction-to-neural-networks-in-python-7e0b422e6c24
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+inputs = np.array([[0, 1, 0],
+                  [0, 1, 1],
+                  [0, 0, 0],
+                  [1, 0, 0],
+                  [1, 1, 1],
+                  [1, 0, 1]])
+
+outputs = np.array([[0], [0], [0], [1], [1], [1]])
+
+
+class NeuralNetwork:
+
+    def __init__(self, inputs, outputs):
+        self.inputs = inputs
+        self.outputs = outputs
+        self.weights = np.array([[0.5], [0.5], [0.5]])  ## each feature in data (each value)
+        self.error_history = []  ## plotting purpose
+        self.epoch_list = []  ## plotting purpose
+
+
+    def sigmoid(self, x, deriv=False):   ## https://pl.wikipedia.org/wiki/Funkcja_aktywacji
+        if deriv == True:
+            return x * 1/x
+        return 1 / (1 + np.exp(-x))   ## function value from sigmoid function - derivative
+
+
+    def feed_forward(self):
+        self.hidden = self.sigmoid(np.dot(self.inputs, self.weights))   ## numpy.dot means product of 2 arrays (input x weight) and after that put to the sigmoid function)
+
+
+    def backpropagation(self):
+        self.error = self.outputs - self.hidden
+        delta = self.error * self.sigmoid(self.hidden, deriv=True)
+        self.weights += np.dot(self.inputs.T, delta)
+
+
+    def train(self, epochs=25000):  ## epochs - iterations
+        for epoch in range(epochs):
+            self.feed_forward()
+            self.backpropagation()
+
+            self.error_history.append(np.average(np.abs(self.error)))
+            self.epoch_list.append(epoch)
+
+
+    def predict(self, new_input):
+        prediction = self.sigmoid(np.dot(new_input, self.weights))
+        return prediction
+
+NN = NeuralNetwork(inputs, outputs)
+
+NN.train()
+
+example = np.array([[1, 1, 0]])
+example_2 = np.array([[0, 1, 1]])
+
+print(NN.predict(example), ' - Correct: ', example[0][0])
+print(NN.predict(example_2), ' - Correct: ', example_2[0][0])
+
+plt.figure(figsize=(15,5))
+plt.plot(NN.epoch_list, NN.error_history)
+plt.xlabel('Epoch')
+plt.ylabel('Loss')
+plt.show()
+
